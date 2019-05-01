2639637cb1853eb081445620013289daacb9da03
xandrucea <ciocea@posteo.de>
2019-05-01 19:45:53 +0200

    remove old website

--- oldWebsite/customjs/testing.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-	$("#button-videos").click(function(){
-		$("#ac-panel-videos").toggleClass('ac-active ac-panel-videos');
-		// $('.ac-panel-videos').toggle("slide", {direction:"right"}, 500);
-		$('#button-videos-i').toggleClass('fa-arrow-right fa-arrow-left');
-	});
-});

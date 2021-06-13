2b0f22f4610b240b8e03ec127a1d9f8ace20fe51
xandrucea <ciocea@posteo.de>
2021-06-13 22:40:04 +0200

    Add gitignore file Add prettier formatter

---
+++ assets/partials/header.php
@@ -0,0 +1,32 @@
+
+
+<!-- Header -->
+<header id="header">
+    <div class="inner">
+        <a href="https://alex-cio.de" class="image avatar"><img src="images/custom/alex_600.png" alt="" /></a>
+        <h1><strong>Ich bin Alex Cio</strong>, <br /> 
+            Onlineaktivist und Filmemacher<br />
+        <hr/>
+        <!-- <hr> -->
+        <div>
+            <a href="?entry=blog" role="button" class="btn btn-primary">
+                mein blog
+            </a>
+        </div>
+        <div>
+            <a href="?entry=buecherregal" role="button" class="btn btn-primary">
+                Bücherregal
+            </a>
+        </div>
+        <div>
+            <a href="?entry=references" role="button" class="btn btn-primary">
+                Ich online
+            </a>
+        </div>
+        <div>
+            <a href="?entry=contact" role="button" class="btn btn-primary">
+                Impressum
+            </a>
+        </div>
+    </div>
+</header>
--- private/processing/sketch3.js
+++ private/processing/sketch3.js
@@ -26,15 +26,14 @@ function setup() {
 function draw() {
   background(255);
   generate();
-  for ( let i = 0; i < columns;i++) {
-    for ( let j = 0; j < rows;j++) {
-      if ((board[i][j] == 1)) fill(0);
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      if (board[i][j] == 1) fill(0);
       else fill(255);
       stroke(0);
-      rect(i * w, j * w, w-1, w-1);
+      rect(i * w, j * w, w - 1, w - 1);
     }
   }
-
 }
 
 // reset board when mouse is pressed
@@ -47,7 +46,8 @@ function init() {
   for (let i = 0; i < columns; i++) {
     for (let j = 0; j < rows; j++) {
       // Lining the edges with 0s
-      if (i == 0 || j == 0 || i == columns-1 || j == rows-1) board[i][j] = 0;
+      if (i == 0 || j == 0 || i == columns - 1 || j == rows - 1)
+        board[i][j] = 0;
       // Filling the rest randomly
       else board[i][j] = floor(random(2));
       next[i][j] = 0;
@@ -57,7 +57,6 @@ function init() {
 
 // The process of creating the new generation
 function generate() {
-
   // Loop through every spot in our 2D array and check spots neighbors
   for (let x = 1; x < columns - 1; x++) {
     for (let y = 1; y < rows - 1; y++) {
@@ -65,7 +64,7 @@ function generate() {
       let neighbors = 0;
       for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
-          neighbors += board[x+i][y+j];
+          neighbors += board[x + i][y + j];
         }
       }
 
@@ -73,10 +72,13 @@ function generate() {
       // we added it in the above loop
       neighbors -= board[x][y];
       // Rules of Life
-      if      ((board[x][y] == 1) && (neighbors <  2)) next[x][y] = 0;           // Loneliness
-      else if ((board[x][y] == 1) && (neighbors >  3)) next[x][y] = 0;           // Overpopulation
-      else if ((board[x][y] == 0) && (neighbors == 3)) next[x][y] = 1;           // Reproduction
-      else                                             next[x][y] = board[x][y]; // Stasis
+      if (board[x][y] == 1 && neighbors < 2) next[x][y] = 0;
+      // Loneliness
+      else if (board[x][y] == 1 && neighbors > 3) next[x][y] = 0;
+      // Overpopulation
+      else if (board[x][y] == 0 && neighbors == 3) next[x][y] = 1;
+      // Reproduction
+      else next[x][y] = board[x][y]; // Stasis
     }
   }
 

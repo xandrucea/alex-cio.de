0837bb31ac57140ae5815a014802879bd6727681
xandrucea <ciocea@posteo.de>
2021-07-04 22:57:11 +0200

    Add banner component

---
+++ assets/partials/bookbanner.php
@@ -0,0 +1,9 @@
+<?php
+$image = "/images/banner.png";
+$altText =
+    "Ein Buch für YouTube Anfänger. Von YouTubern für YouTuber von Manuel Fiedler und Alex Cio";
+?>
+
+<div id="book-banner">
+    <a href="https://amzn.to/3dIaReA" rel="noreferrer noopener"><img src="<?= $image ?>" alt="<?= $altText ?>" /></a>
+</div>
